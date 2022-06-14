5ba9c97d4423a89329143a6e307b131b2eab1fad
ethilty <spinycucumber@gmail.com>
2022-06-14 17:08:43 +0100

    Phew

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const fs = require('fs/promises')
+
+LAST_RESET_PATH = process.env.LAST_RESET_PATH || './last-reset.json'
+let lastReset = fs.readFile(LAST_RESET_PATH).then(JSON.parse)
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send({ message: 'Hello world' });
+router.post('/reset', (req, res) => {
+    lastReset = Promise.resolve({
+        time: Date.now(),
+        reason: req.body.reason
+    });
+    // Save last reset
+    lastReset.then(JSON.stringify).then((data) => {
+        fs.writeFile(LAST_RESET_PATH, data);
+    });
+    res.sendStatus(200);
+});
+
+router.get('/lastreset', (req, res) => {
+    lastReset.then((result) => {
+        res.send(result);
+    });
 });
 
 module.exports = router;
